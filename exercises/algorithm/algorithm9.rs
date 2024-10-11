d4314048c3f7218cf60b145b7e4cddf186bad637
ddcat <2164421436@qq.com>
2024-10-12 01:14:24 +0800

    1

--- exercises/algorithm/algorithm9.rs
+++ exercises/algorithm/algorithm9.rs
@@ -2,12 +2,14 @@
 	heap
 	This question requires you to implement a binary heap function
 */
-// I AM NOT DONE
+
 
 use std::cmp::Ord;
 use std::default::Default;
+use std::mem::swap;
+use std::process::id;
 
-pub struct Heap<T>
+pub struct Heap<T: Clone>
 where
     T: Default,
 {
@@ -18,7 +20,7 @@ where
 
 impl<T> Heap<T>
 where
-    T: Default,
+    T: Default + std::clone::Clone,
 {
     pub fn new(comparator: fn(&T, &T) -> bool) -> Self {
         Self {
@@ -38,8 +40,41 @@ where
 
     pub fn add(&mut self, value: T) {
         //TODO
+        self.items.push(value);
+        self.count+=1;
+        self.upper(self.count);
     }
+    fn upper(&mut self,idx: usize){
+        if &self.parent_idx(idx)>=&1&&!(self.comparator)(&self.items[self.parent_idx(idx)],&self.items[idx]){
+            let p=self.parent_idx(idx).clone();
+            //swap(&mut self.items[p], &mut self.items[idx]);
+            let tp:T=self.items.get(p).unwrap().clone();
+            self.items[p]=self.items[idx].clone();
+            self.items[idx]=tp.clone();
 
+            println!("change: idx:{} p:{} ",idx,self.parent_idx(idx));
+            //println!("after change:idx:{} ,p:{}",self.items[idx].clone(),self.items[p].clone());
+            self.upper(self.parent_idx(idx));
+        }
+    }
+    fn down(&mut self,idx:usize){
+        let l=self.left_child_idx(idx);
+        let r=self.right_child_idx(idx);
+        let mut to=idx.clone();
+        if l<=self.count&&(self.comparator)(&self.items[l],&self.items[to]){
+            to=l.clone();
+        }
+        if r<=self.count&&(self.comparator)(&self.items[r],&self.items[to]){
+            to=r.clone();
+        }
+        if to!=idx{
+            //swap(&mut self.items[to],&mut self.items[idx]);
+            let tp=self.items.get(to).unwrap().clone();
+            self.items[to]=self.items.get(idx).unwrap().clone();
+            self.items[idx]=tp;
+            self.down(to);
+        }
+    }
     fn parent_idx(&self, idx: usize) -> usize {
         idx / 2
     }
@@ -58,13 +93,13 @@ where
 
     fn smallest_child_idx(&self, idx: usize) -> usize {
         //TODO
-		0
+        1
     }
 }
 
 impl<T> Heap<T>
 where
-    T: Default + Ord,
+    T: Default + Ord + std::clone::Clone,
 {
     /// Create a new MinHeap
     pub fn new_min() -> Self {
@@ -79,13 +114,21 @@ where
 
 impl<T> Iterator for Heap<T>
 where
-    T: Default,
+    T: Default + std::clone::Clone,
 {
     type Item = T;
 
     fn next(&mut self) -> Option<T> {
         //TODO
-		None
+        if self.is_empty() {
+            return None;
+        }
+        let t=self.items[1].clone();
+        self.items[1]=self.items[self.count].clone();
+        self.items.remove(self.count);
+        self.count-=1;
+        self.down(1);
+        Some(t.clone())
     }
 }
 
@@ -95,7 +138,7 @@ impl MinHeap {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<T>() -> Heap<T>
     where
-        T: Default + Ord,
+        T: Default + Ord + std::clone::Clone,
     {
         Heap::new(|a, b| a < b)
     }
@@ -107,7 +150,7 @@ impl MaxHeap {
     #[allow(clippy::new_ret_no_self)]
     pub fn new<T>() -> Heap<T>
     where
-        T: Default + Ord,
+        T: Default + Ord + std::clone::Clone,
     {
         Heap::new(|a, b| a > b)
     }
